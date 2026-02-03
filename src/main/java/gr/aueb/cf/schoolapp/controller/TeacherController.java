58877c80f6e02b054c9e6098d67252d4fe118842
a8anassis <a8anassis@gmail.com>
2026-02-03 20:58:47 +0200

    feat: add / remove features for teacher insert

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -2,12 +2,16 @@ package gr.aueb.cf.schoolapp.controller;
 
 import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
-import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -20,6 +24,30 @@ public class TeacherController {
         model.addAttribute("teacherInsertDTO", TeacherInsertDTO.empty());
         // model.addAttribute("regionsReadOnlyDTO", regions());
         return "teacher-insert";
+    }
+
+    @PostMapping("/insert")
+    public String teacherInsert(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO teacherInsertDTO,
+                                BindingResult bindingResult, Model model,
+                                RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            // model.addAttribute("regionsReadOnlyDTO", regions());
+            return "teacher-insert";
+        }
+
+        // save teacher to DB
+        TeacherReadOnlyDTO teacherReadOnlyDTO = new TeacherReadOnlyDTO("acfd-1234", "Αθανάσιος", "Ανδρούτσος");
+        //model.addAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+
+        // PRG - Post-Redirect-Get
+        redirectAttributes.addFlashAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+        return "redirect:/teachers/success";
+    }
+
+    @GetMapping("/success")
+    public String teacherSuccess(Model model) {
+        return "teacher-success";
     }
 
 

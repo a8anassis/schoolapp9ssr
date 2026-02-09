de243d42b983fd78857ba5f1a8a05bfcbad58cb8
a8anassis <a8anassis@gmail.com>
2026-02-09 21:07:38 +0200

    fix: add min to @Size

--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
@@ -10,13 +10,13 @@ public record TeacherInsertDTO(
         //        @Size(min = 2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
 
         @NotNull
-        @Size
+        @Size(min = 2)
         String firstname,
 
 //        @NotNull(message = "Το επώνυμο δεν μπορεί να είναι κενό")
 //        @Size(min = 2, message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
         @NotNull
-        @Size
+        @Size(min = 2)
         String lastname,
 
 //        @Pattern(regexp = "\\d{9,}", message = "Το ΑΦΜ δεν μπορεί να είναι μικρότερο από εννέα ψηφία.")

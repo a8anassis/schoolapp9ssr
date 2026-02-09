19889c94d0cb2d7510ae8f0ebc1043d4b6334767
a8anassis <a8anassis@gmail.com>
2026-02-09 20:47:01 +0200

    chore: add localization logic

--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
@@ -6,18 +6,25 @@ import jakarta.validation.constraints.Size;
 
 public record TeacherInsertDTO(
 
-        @NotNull(message = "Το όνομα δεν μπορεί να είναι κενό")
-        @Size(min = 2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
+//        @NotNull(message = "Το όνομα δεν μπορεί να είναι κενό")
+        //        @Size(min = 2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
+
+        @NotNull
+        @Size
         String firstname,
 
-        @NotNull(message = "Το επώνυμο δεν μπορεί να είναι κενό")
-        @Size(min = 2, message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
+//        @NotNull(message = "Το επώνυμο δεν μπορεί να είναι κενό")
+//        @Size(min = 2, message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
+        @NotNull
+        @Size
         String lastname,
 
-        @Pattern(regexp = "\\d{9,}", message = "Το ΑΦΜ δεν μπορεί να είναι μικρότερο από εννέα ψηφία.")
+//        @Pattern(regexp = "\\d{9,}", message = "Το ΑΦΜ δεν μπορεί να είναι μικρότερο από εννέα ψηφία.")
+        @Pattern(regexp = "\\d{9,}")
         String vat,
 
-        @NotNull(message = "Η περιοχή δεν μπορεί να είναι κενή.")
+//        @NotNull(message = "Η περιοχή δεν μπορεί να είναι κενή.")
+        @NotNull
         Long regionId
 ) {
     public static TeacherInsertDTO empty() {
